b842bae3ccb68019275dc90db64ea26c315f6019
ludralph <raphaelumoh@gmail.com>
2023-09-01 15:01:48 +0100

    add PORT variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 app.set('view engine', 'ejs');
 
